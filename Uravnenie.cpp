3756f941f054eed3e2f98fb2151d3dcf9ef5e4a3
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-10 15:14:20 +0300

    Create Uravnenie.cpp

---
+++ Uravnenie.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, b;
+    cout << "Введите значение x\n";
+    cin >> a;
+    b = 3*pow(a,6) - 6*pow(a,3) - 7;
+    cout << b;
+    return 0;
+}
